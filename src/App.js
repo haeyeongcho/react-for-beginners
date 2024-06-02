420851551be1527afc94b70a085bf06d5f43e818
socilt54 <socilt54@gmail.com>
2024-06-02 19:54:17 +0900

    css module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <button onClick={onClick}>Click me</button>
+      <p>{counter}</p>
+      <Button text={"Continue"} />
     </div>
   );
 }
