9d084d534665df08632336869f580ebf3bde7975
socilt54 <socilt54@gmail.com>
2024-06-03 18:18:56 +0900

    Publishing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/hello" element={<h1>hello</h1>} />
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
