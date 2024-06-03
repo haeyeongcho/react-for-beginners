7358e9d0993142e2c3430938419a9b644cf70527
socilt54 <socilt54@gmail.com>
2024-06-03 13:47:12 +0900

    Dollar -> btc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [amountUSD, setAmountUSD] = useState("");
+  const [btcPrice, setBtcPrice] = useState(0);
+  const [howMuchBTC, setHowMuchBTC] = useState(0);
+  const onChange = (event) => {
+    const value = event.target.value;
+    setAmountUSD(value);
+    setHowMuchBTC(value / 68412);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
       response.json().then((json) => {
@@ -16,16 +24,26 @@ function App() {
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
-            </option>
-          ))}
-        </select>
+        <>
+          <input
+            value={amountUSD}
+            type="number"
+            onChange={onChange}
+            placeholder="Enter amount in USD"
+          ></input>
+          <div>{howMuchBTC} BTC</div>
+          <select>
+            {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
+              </option>
+            ))}
+          </select>
+        </>
       )}
     </div>
   );
